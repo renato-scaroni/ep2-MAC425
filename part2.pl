4b260d9e7016bee2b7004b49b864fed7072ecf3c
renato-scaroni <scaroni@scaroni-N56VJ.(none)>
2013-11-23 23:24:06 -0200

    tentativas de fazer a leitura de entrada, mas esse prolog eh nojento

--- part2.pl
+++ part2.pl
@@ -1,8 +1,28 @@
-animal(dog).
+% 46 eh .
+% 32 eh espaco
+
+ultimo_elem(X, [E1|T]) :- X = E1, T = [].
+ultimo_elem(X, [E1|T]) :- ultimo_elem(X, T).
+
+append([], List, List).
+append([Head|Tail], List, [Head|Rest]) :- append(Tail, List, Rest).
+
+read_line(L) :- get0(C), char_code_list(C,L).
+
+char_code_list(10,[]) :- !.
+char_code_list(C,[C|X]) :- get0(C2), char_code_list(C2,X).
 
 
-read_animal(X) :-
-  write('please type animal name:'),
-  nl,
-  read(X),
+is_word(X) :- ultimo_elem(Y, X), Y = 32.
+is_word(X) :- ultimo_elem(Y, X), Y = 46.
+
+get_word(S, X) :- is_word(X), is_subset(S, X).
+
+is_subset([E1|T1], [E2|T2]) :- E1 = E2, is_subset(T1, T2).
+is_subset([E1|T1], [E2|T2]) :- E1 \= E2, is_subset(T1, [E2|T2]).
+is_subset(T1, T2) :- T2 = [].
+
+monta_lista(X) :-
+  read_line(X),
+  %atom_codes(Y, X),
   write(X).
